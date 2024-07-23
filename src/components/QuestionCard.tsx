d8262aa9f950462b197f9a495bd4bb75021f2242
monasubedi <sbd.mona@gmail.com>
2024-07-23 10:56:08 -0400

    add quiz app

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { AnswerObject } from "../App";
+import { ButtonWrapper, Wrapper } from "./QuestionCard.styles";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}: Props) => {
+  return (
+    <Wrapper>
+      <p className="number">
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer, ind) => (
+          <ButtonWrapper
+            key={ind}
+            correct={userAnswer?.correctAnswer === answer}
+            userClicked={userAnswer?.answer === answer}
+          >
+            <button disabled={!!userAnswer} value={answer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </ButtonWrapper>
+        ))}
+      </div>
+    </Wrapper>
+  );
+};
+
+export default QuestionCard;
